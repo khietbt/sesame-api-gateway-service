3ea49b87397c169f2e40bf6532e94f2ce22fc637
khietbt <khietbt@gmail.com>
2023-10-25 06:43:37 +0000

    Initial commit

---
+++ src/main/java/io/github/khietbt/SesameApiGatewayService.java
@@ -0,0 +1,12 @@
+package io.github.khietbt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SesameApiGatewayService {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SesameApiGatewayService.class, args);
+    }
+}
