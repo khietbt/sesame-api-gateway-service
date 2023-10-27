7957c6d296346b22aaca7b0cf9b956ed6724bfb2
khietbt <khietbt@gmail.com>
2023-10-27 06:05:34 +0000

    Make this module work like a resource server

--- src/main/java/io/github/khietbt/SesameApiGatewayService.java
+++ src/main/java/io/github/khietbt/SesameApiGatewayService.java
@@ -2,8 +2,10 @@ package io.github.khietbt;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 
 @SpringBootApplication
+@EnableWebFluxSecurity
 public class SesameApiGatewayService {
 
     public static void main(String[] args) {
